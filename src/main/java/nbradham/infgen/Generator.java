e93fa702c3aa5776fd055e132d0be0a3eb7de7b9
nickolasbradham <nickolasbradham@gmail.com>
2022-10-13 14:46:53 -0600

    Initial setup.

--- src/main/java/nbradham/infgen/Generator.java
+++ src/main/java/nbradham/infgen/Generator.java
@@ -1,5 +1,7 @@
 package nbradham.infgen;
 
+import javax.swing.JOptionPane;
+
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
@@ -7,10 +9,37 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
 final class Generator implements NativeKeyListener {
 
+	private static enum Dir {
+		UP(0, 1), RIGHT(1, 0), DOWN(0, -1), LEFT(-1, 0);
+
+		private final int dx, dy;
+
+		private Dir(int x, int y) {
+			dx = x;
+			dy = y;
+		}
+	};
+
+	private byte r, skpX, skpY;
+
+	private Generator(byte radius, byte skipX, byte skipY) {
+		r = radius;
+		skpX = skipX;
+		skpY = skipY;
+	}
+
 	private void start() throws NativeHookException {
 		GlobalScreen.registerNativeHook();
 		GlobalScreen.addNativeKeyListener(this);
-		//TODO Everything else.
+
+		JOptionPane.showMessageDialog(null, "Ready");
+
+		byte x = 0, y = 0, t = 1;
+		Dir d = Dir.UP;
+		while (x != skpX && y != skpY) {
+			// TODO Continue code.
+		}
+
 		GlobalScreen.unregisterNativeHook();
 	}
 
@@ -21,10 +50,22 @@ final class Generator implements NativeKeyListener {
 	 */
 	@Override
 	public void nativeKeyTyped(NativeKeyEvent nativeEvent) {
-
+		// TODO Handle.
 	}
 
 	public static void main(String[] args) throws NativeHookException {
-		new Generator().start();
+		if (!(args.length == 1 || args.length == 3)) {
+			System.out.println(
+					"Arguments: <radius> [<skipX> <skipY>]\n  radius - Chunk radius to generate.\n  skipX - Generator region skip X.\n skipY = Generator region skip Y.");
+			return;
+		}
+
+		byte x = 0, y = 0;
+		if (args.length > 1) {
+			x = Byte.parseByte(args[1]);
+			y = Byte.parseByte(args[2]);
+		}
+
+		new Generator(Byte.parseByte(args[0]), x, y).start();
 	}
 }
