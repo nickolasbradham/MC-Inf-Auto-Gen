e2b43e06da4f846ed113337fbd079cbc3a23c9b9
nickolasbradham <nickolasbradham@gmail.com>
2022-11-24 12:35:56 -0700

    Removed unused config settings.

--- src/main/java/nbradham/infgen/Config.java
+++ src/main/java/nbradham/infgen/Config.java
@@ -15,11 +15,9 @@ import java.util.Properties;
  */
 final class Config {
 	private static final String K_BUT_X = "buttonX", K_SP_Y = "singleplayerY", K_W1_Y = "world1Y",
-			K_MEN_X = "menuPixelX", K_MEN_HUE = "menuHue", K_QUIT_HUE = "quitHue", K_CENTER_X = "centerX",
-			K_CENTER_Y = "centerY", K_HB_X = "hotbarX", K_HB_Y = "hotbarY", K_HB_H = "hotbarHue";
+			K_MEN_X = "menuPixelX", K_MEN_HUE = "menuHue", K_QUIT_HUE = "quitHue", K_HB_X = "hotbarX", K_HB_Y = "hotbarY", K_HB_H = "hotbarHue";
 	private static final File FILE = new File("config.cfg");
 	static float menuHue, quitHue, hbh;
-	static int mouseX, mouseY;
 	static short buttonX, sp, w1, menuX, hbx, hby;
 
 	/**
@@ -47,8 +45,6 @@ final class Config {
 		p.putIfAbsent(K_MEN_X, "700");
 		p.putIfAbsent(K_MEN_HUE, ".07");
 		p.putIfAbsent(K_QUIT_HUE, ".64");
-		p.putIfAbsent(K_CENTER_X, "960");
-		p.putIfAbsent(K_CENTER_Y, "552");
 		p.putIfAbsent(K_HB_X, "790");
 		p.putIfAbsent(K_HB_Y, "750");
 		p.putIfAbsent(K_HB_H, ".3");
@@ -61,8 +57,6 @@ final class Config {
 		menuX = Short.parseShort(p.getProperty(K_MEN_X));
 		menuHue = Float.parseFloat(p.getProperty(K_MEN_HUE));
 		quitHue = Float.parseFloat(p.getProperty(K_QUIT_HUE));
-		mouseX = Short.parseShort(p.getProperty(K_CENTER_X)) + 2;
-		mouseY = Short.parseShort(p.getProperty(K_CENTER_Y));
 		hbx = Short.parseShort(p.getProperty(K_HB_X));
 		hby = Short.parseShort(p.getProperty(K_HB_Y));
 		hbh = Float.parseFloat(p.getProperty(K_HB_H));
